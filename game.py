cf2f6bbd82e1cd4924a420df4810f2923a39ccdf
“Adam“ <adamwang@college.harvard.edu>
2022-11-28 17:50:22 -0500

    game template1

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
-print("testing")
+# import and initialize
+import pygame
+pygame.init()
+
+# game window
+screen = pygame.display.set_mode([500, 500])
+
+# run until user quits
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # background
+    screen.fill((255, 255, 255))
+
+pygame.quit()
